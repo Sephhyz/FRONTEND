f8a238a761b325e18440ed2eafd03b5105c92a52
unisanta <unisanta@santacecilia.br>
2023-04-27 10:22:22 -0300

    FRONTEND

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+import { Tarefa } from "./tarefa";
+
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+  apiURL : string;
+  constructor(private http: HttpClient) {
+    this.apiURL = 'https://pedro214897.onrender.com';
+  this.READ_tarefas();
+  }
+  
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+  this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
+ resultado => { console.log(resultado); this.READ_tarefas(); });
+    }
+   
+  READ_tarefas() {
+  this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
+ resultado => this.arrayDeTarefas=resultado);
+  }
+
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
+    resultado => { console.log(resultado); this.READ_tarefas(); });   
+
+  }
+
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
+    var id = this.arrayDeTarefas[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
+    tarefaAserModificada).subscribe(
+    resultado => { console.log(resultado); this.READ_tarefas(); });
+   }
+  
+
+
+ 
+}
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Tarefa } from "../tarefa";
+@Component({
+ selector: 'app-item',
+ templateUrl: './item.component.html',
+ styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+ emEdicao = false;
+ @Input() tarefa: Tarefa = new Tarefa("", false);
+ @Output() removeTarefa = new EventEmitter();
+ @Output() modificaTarefa = new EventEmitter();
+
+}
+
+
